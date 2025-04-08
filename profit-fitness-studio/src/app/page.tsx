e80a01061547df4ba6000d337da0faa6d04264ba
sai_say_noom_leng-dev <snoomleng1@cougars.ccis.edu>
2025-04-08 11:18:30 -0500

    finished banner

--- profit-fitness-studio/src/app/page.tsx
+++ profit-fitness-studio/src/app/page.tsx
@@ -1,9 +1,11 @@
+import Banner from '@/components/Banner';
 import Hero from '@/components/ui/Hero';
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Banner />
     </main>
   );
 }
